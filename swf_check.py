668167ec74e11b649bb11d4137932a64e8dddc3d
izhar <gk1042mq@metrostate.edu>
2018-08-01 15:35:43 -0500

    commented code

--- swf_check.py
+++ swf_check.py
@@ -1,23 +1,22 @@
 #!/usr/bin/env python
 
-# SWFFile take in a file directory or file that is in the same path being called and checks weither 
-# it is compressed or not compressed then extracts the given values from the swf files
+# SWFFile takes in a directory or a single file and checks whether it is compressed or 
+# not compressed, then extracts the embedded links from the swf file(s) and outputs 
+# them to all_swf_files.txt
 
 import subprocess
 from subprocess import call, check_output, Popen
 from glob import glob
 
-#swf_file  = "filename.swf"
 dump_file = "dump_swf.txt"
 all_file_dump = "all_swf_files.txt"
 filename = " "
 check_loop = 0
-
 finished_reading_file = False
-
 read_count = 0
 count = 0
 
+# trid checks whether a single file is uncompressed or needs to be decompressed 
 def trid(file):
 	global check_loop
 	global filename
@@ -33,28 +32,29 @@ def trid(file):
 		if word == "Compressed":
 			print("----------------- File is Compressed -----------------\n")
 			check_loop = 1
-	#checks file to right position based on if it is compressed or not		
+	# checks file to right position based on if it is compressed or not		
 	if check_loop == 1: 
 		flasm()
 	else:
 		print("----------------- File is NOT compressed ---------------\n")
 		swfDump()
 
+# if file is compressed flasm is called to decompress the file
 def flasm():
 	global filename
 	print("Decompressing...\n")
 	# decompress it
 	call(["flasm", "-x", filename])
 
-	#chasnge the filetype
+	# chasnge the filetype
 	change_file = list(filename)
 	print(change_file)
 	change_file[-3] = 's'
-	#print(change_file)
 	filename = "".join(change_file)
 
 	swfDump()
 
+# saves links found to a text file
 def swfDump():
 	print("Dumping...\n")
 
@@ -70,37 +70,31 @@ def swfDump():
 			if "http" in letter:
 				print(("{} \n").format(letter))
 				outf.write(("{}\n\n").format(letter))
-	#print(finished_reading_file)
 	if finished_reading_file :
 		print("Finished!\n\ndump_swf.txt is in terminal directory\n")
-		#print(count)
-		#print(read_count)
-	#else:
-	#	loopFiles()
 
 def loopFiles():
 	global finished_reading_file
 	global read_count
 
-	#make in to array
-	#read file for passing in value
+	# make in to array
+	# read file for passing in value
 	with open(all_file_dump, "r") as read_file:
 		for line in read_file:
 			read_count+=1
 			file_line = line.strip("\n")
-			#print("in loop")
 			print(line.strip("\n"))
-			
+
 			if count == read_count:
 				finished_reading_file = True
 			trid(file_line)
 	
 def find(file_dir):
 	global count
-	#print("dir---------------")
 	print(file_dir)
 	with open(all_file_dump, "a") as out_file:
 		print("Files Found:\n")
+
 		for line in file_dir:
 			count+=1
 			out_file.write(("{}\n").format(line))
@@ -109,8 +103,7 @@ def find(file_dir):
 
 def main():
 	global finished_reading_file
-	
-	#add error detection
+	# add error detection
 
 	x = int(raw_input("enter 1 for one file or 0 for many: "))
 	open(all_file_dump, "w").close()
@@ -125,12 +118,8 @@ def main():
 		file_dir = glob(("{}/*.$wf").format(all))
 		find(file_dir)
 		loopFiles()
-		#file_dir = [glob(i) for i in [('{}/*.swf').format(all), ('{}/*.$wf').format(all)]]
-		#write all file to txt file
-		
 	else:
 		print("Only 1 or 0 for input!")
 		main()
-
 
 main()
